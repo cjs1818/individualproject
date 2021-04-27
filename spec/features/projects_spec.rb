b2ec087cfec80cc161cfb7674ce4f88b9953a79b
Chandler Stone <cstone3@uccs.edu>
2021-04-27 09:41:20 +0000

    Fixed tests

--- spec/features/projects_spec.rb
+++ spec/features/projects_spec.rb
@@ -5,19 +5,28 @@ RSpec.feature "Projects", type: :feature do
     before(:each) do
       visit new_project_path
       within("form") do
-        fill_in "Title", with: "Test title"
+        #fill_in "Title", with: "Test title"
       end
     end
 
     scenario "should be successful" do
-      fill_in "Username", with: "Test username"
-      fill_in "Password", with: "Test password"
+      click_link "Sign up"
+      fill_in "Email", with: "test@email.com"
+      fill_in "Password", with: "password"
+      fill_in "Password confirmation", with: "password"
+      click_button "Sign up"
+      fill_in "Title", with: "Test title"
       fill_in "Description", with: "Test description"
       click_button "Create Project"
       expect(page).to have_content("Project was successfully created")
     end
 
     scenario "should fail" do
+      click_link "Sign up"
+      fill_in "Email", with: "test@email.com"
+      fill_in "Password", with: "password"
+      fill_in "Password confirmation", with: "password"
+      click_button "Sign up"
       click_button "Create Project"
       expect(page).to have_content("Description can't be blank")
     end
@@ -30,6 +39,11 @@ RSpec.feature "Projects", type: :feature do
     end
 
     scenario "should be successful" do
+      click_link "Sign up"
+      fill_in "Email", with: "test@email.com"
+      fill_in "Password", with: "password"
+      fill_in "Password confirmation", with: "password"
+      click_button "Sign up"
       within("form") do
         fill_in "Description", with: "New description content"
       end
@@ -38,6 +52,11 @@ RSpec.feature "Projects", type: :feature do
     end
 
     scenario "should fail" do
+      click_link "Sign up"
+      fill_in "Email", with: "test@email.com"
+      fill_in "Password", with: "password"
+      fill_in "Password confirmation", with: "password"
+      click_button "Sign up"
       within("form") do
         fill_in "Description", with: ""
       end
@@ -50,6 +69,12 @@ RSpec.feature "Projects", type: :feature do
     let!(:project) { Project.create(title: "Test title", description: "Test content") }
     scenario "remove project" do
       visit projects_path
+      click_link "Destroy"
+      click_link "Sign up"
+      fill_in "Email", with: "test@email.com"
+      fill_in "Password", with: "password"
+      fill_in "Password confirmation", with: "password"
+      click_button "Sign up"
       click_link "Destroy"
       expect(page).to have_content("Project was successfully destroyed")
       expect(Project.count).to eq(0)
